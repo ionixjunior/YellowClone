8c5ee51eedd5a4c04a4bf5d01645257f223c912e
Ione Souza Junior <junior@ionixjunior.com.br>
2022-09-14 23:33:53 -0300

    Adicionar MAUI Program

---
+++ YellowClone/MauiProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace YellowClone
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>();
+
+            return builder.Build();
+        }
+    }
+}
+
