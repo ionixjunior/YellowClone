eb14ff949d3a2bd7ffc07bbc4512838555181ddc
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-11-23 21:59:47 -0300

    Ajustes no código

--- YellowClone/Controls/CustomMap.ios.cs
+++ YellowClone/Controls/CustomMap.ios.cs
@@ -1,7 +1,7 @@
-﻿#if IOS
-using System;
+﻿using System;
 using Foundation;
 using MapKit;
+using Microsoft.Maui.Maps;
 using Microsoft.Maui.Maps.Handlers;
 using UIKit;
 
@@ -39,8 +39,6 @@ public class PlatformCustomMap : Microsoft.Maui.Maps.Platform.MauiMKMapView
 
     new MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
     {
-        MKAnnotationView annotationView = null;
-
         if (annotation is MKUserLocation)
             return null;
 
@@ -51,13 +49,14 @@ public class PlatformCustomMap : Microsoft.Maui.Maps.Platform.MauiMKMapView
 
         var markerId = customPin.MarkerId.ToString();
 
-        annotationView = mapView.DequeueReusableAnnotation(markerId);
-
+        var annotationView = mapView.DequeueReusableAnnotation(markerId);
         if (annotationView is null)
         {
-            annotationView = new CustomMKAnnotationView(annotation, markerId);
-            annotationView.Image = UIImage.FromFile("map_pin.png");
-            annotationView.CalloutOffset = new CoreGraphics.CGPoint(0, 0);
+            annotationView = new CustomMKAnnotationView(annotation, markerId)
+            {
+                Image = UIImage.FromFile("map_pin.png"),
+                CalloutOffset = new CoreGraphics.CGPoint(0, 0)
+            };
 
             ((CustomMKAnnotationView)annotationView).Id = customPin.Identifier.ToString();
             ((CustomMKAnnotationView)annotationView).Information = customPin.Information;
@@ -127,5 +126,3 @@ public class PlatformCustomMap : Microsoft.Maui.Maps.Platform.MauiMKMapView
         }
     }
 }
-
-#endif
