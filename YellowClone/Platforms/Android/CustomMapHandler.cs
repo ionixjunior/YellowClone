30d79528d485e3b9d295ba61e41340ec409110c1
Ione Souza Junior <junior@ionixjunior.com.br>
2022-12-12 22:49:52 -0300

    Exibir pins com ícone customizado no Android

--- YellowClone/Platforms/Android/CustomMapHandler.cs
+++ YellowClone/Platforms/Android/CustomMapHandler.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
 using Microsoft.Maui.Maps.Handlers;
+using Microsoft.Maui.Platform;
 using IMap = Microsoft.Maui.Maps.IMap;
 
 namespace YellowClone.Platforms.Android
@@ -9,6 +14,7 @@ namespace YellowClone.Platforms.Android
         public static readonly IPropertyMapper<IMap, IMapHandler> CustomMapper =
             new PropertyMapper<IMap, IMapHandler>(Mapper)
             {
+                [nameof(IMap.Pins)] = MapPins,
             };
 
         public CustomMapHandler() : base(CustomMapper, CommandMapper)
@@ -17,6 +23,40 @@ namespace YellowClone.Platforms.Android
 
         public CustomMapHandler(IPropertyMapper? mapper = null, CommandMapper? commandMapper = null) : base(mapper ?? CustomMapper, commandMapper ?? CommandMapper)
         {
+        }
+
+        protected override void ConnectHandler(MapView platformView)
+        {
+            base.ConnectHandler(platformView);
+            var mapReady = new MapCallbackHandler(this);
+            PlatformView.GetMapAsync(mapReady);
+        }
+
+        private static new void MapPins(IMapHandler handler, IMap map)
+        {
+            if (handler is CustomMapHandler mapHandler)
+            {
+                mapHandler.AddPins(map.Pins);
+            }
+        }
+
+        private void AddPins(IList<Microsoft.Maui.Maps.IMapPin> pins)
+        {
+            if (Map is null || MauiContext is null)
+            {
+                return;
+            }
+
+            foreach (var pin in pins)
+            {
+                var pinHandler = pin.ToHandler(MauiContext);
+                if (pinHandler is IMapPinHandler mapPinHandler)
+                {
+                    var markerOption = mapPinHandler.PlatformView;
+                    markerOption.SetIcon(BitmapDescriptorFactory.FromResource(Resource.Drawable.map_pin));
+                    Map.AddMarker(markerOption);
+                }
+            }
         }
     }
 }
---
+++ YellowClone/Platforms/Android/MapCallbackHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.Gms.Maps;
+using Microsoft.Maui.Maps.Handlers;
+using IMap = Microsoft.Maui.Maps.IMap;
+
+namespace YellowClone.Platforms.Android
+{
+	public class MapCallbackHandler : Java.Lang.Object, IOnMapReadyCallback
+    {
+        private readonly IMapHandler _mapHandler;
+
+        public MapCallbackHandler(IMapHandler mapHandler)
+		{
+            _mapHandler = mapHandler;
+		}
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            _mapHandler.UpdateValue(nameof(IMap.Pins));
+        }
+    }
+}
+
